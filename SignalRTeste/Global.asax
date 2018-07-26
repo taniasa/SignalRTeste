fe8ddb1b609bffef55b906634e98ac88c8236747
Tânia Salgado de Abreu <tania.salgado.ti@hotmail.com>
2018-07-26 14:14:13 -0300

    Inicial, no Redis.

---
+++ SignalRTeste/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SignalRTeste.MvcApplication" Language="C#" %>
---
+++ SignalRTeste/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalRTeste.Startup))]
+
+namespace SignalRTeste
+{
+    public class Startup
+    {
+        private static string Get()
+        {
+            return ConfigurationManager.AppSettings["redis"] ?? "localhost2";
+        }
+        private static string _redis => Get();
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalHost.DependencyResolver.UseRedis(_redis, 6379, "", "ChatRedis");
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.MapSignalR();
+            GlobalHost.HubPipeline.RequireAuthentication();
+        }
+    }
+}
